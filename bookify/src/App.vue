bb5cf16e94d8f414d89117b23b3003ef97294b6e
Grzegorz Wójcik <hirashi7@gmail.com>
2019-06-24 23:34:52 +0200

    init + home

---
+++ bookify/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+    $END$
+</template>
+
+<script>
+    import {Component, Prop, Vue} from 'vue-property-decorator';
+
+    @Component
+    export default class App
+    .vue
+    extends
+    Vue
+    {
+
+    }
+</script>
---
+++ bookify/src/components/Header.vue
@@ -0,0 +1,12 @@
+<template>
+    $END$
+</template>
+
+<script>
+    import {Component, Prop, Vue} from 'vue-property-decorator';
+
+    @Component
+    export default class Header extends Vue {
+
+    }
+</script>
